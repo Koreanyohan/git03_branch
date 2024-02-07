9c4bb65ade4e088230f5aff52484cd6ff2f492af
yohan <ju9580@gmail.com>
2024-02-07 12:43:53 +0900

    created on 07th feb 2024

---
+++ src/main/java/com/example/demo/Git03BranchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git03BranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git03BranchApplication.class, args);
+	}
+
+}
